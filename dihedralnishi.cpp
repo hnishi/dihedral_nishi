17fef5cddaf7ae90875ff942af125a12acfd9c23
U-arakawa-asus\arakawa <arakawa@arakawa-asus.(none)>
2015-04-16 19:48:42 +0900

    ultra

--- dihedralnishi.cpp
+++ dihedralnishi.cpp
@@ -1,7 +1,6 @@
 #include"nlib.h"
 #include"math_nishi.h"
-#include<Eigen/Dense>  // Eigen/Core, Geometry, LU, Cholesky, SVD, QR and Eigen
-values
+#include<Eigen/Dense>  // Eigen/Core, Geometry, LU, Cholesky, SVD, QR and Eigen values
 
 using namespace Eigen;
 
@@ -12,7 +11,9 @@ int dihedralnishi( Inp_nishi inp1 ){
   cout<<"input file name = "<<inp1.filename <<endl;
 
   Vector3f vec1; // initial value is (1, 0, 0)
-  cout<<" vec1."
+  //vec1 << 1.0f, 1.0f, 1.0f;
+  vec1 << 1.0, 1.0, 1.0;
+  cout<<" vec1.norm() = "<<vec1.norm()<<endl;
 
   //pdb_nishi pdb1(bfcod,bfpdb);
   return 0;
