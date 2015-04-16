710f199eafb66cfd437688544379c159cf557acd
hnishi <nishigami2509@gmail.com>
2015-04-16 18:53:14 +0900

    add dih

---
+++ dihedralnishi.cpp
@@ -0,0 +1,19 @@
+#include"nlib.h"
+#include"math_nishi.h"
+#include<Eigen/Dense>  // Eigen/Core, Geometry, LU, Cholesky, SVD, QR and Eigen
+values
+
+using namespace Eigen;
+
+
+int dihedralnishi( Inp_nishi inp1 ){
+  cout<<"Program dihedralnishi( inp1 ) starts!!!\n";
+	
+  cout<<"input file name = "<<inp1.filename <<endl;
+
+  Vector3f vec1; // initial value is (1, 0, 0)
+  cout<<" vec1."
+
+  //pdb_nishi pdb1(bfcod,bfpdb);
+  return 0;
+}
